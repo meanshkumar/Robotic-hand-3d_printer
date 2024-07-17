518a0cb3ccc21a80d9ede2688c811f233b8f0fa9
Ansh Kumar <im.ansh1410@gmail.com>
2024-07-17 07:15:02 +0530

    Added all files created for the final year project

---
+++ Inmoov_Robot_Hand.ino
@@ -0,0 +1,310 @@
+/*
+   Arduino Robotic Hand Tutorial 
+   by Kuldip.K Gami, https://www.youtube.com/channel/UCZlqIQXSSAsdD7mZj2Ret3w
+*/
+
+#include<Servo.h>
+#include<SoftwareSerial.h>
+
+Servo Thumb, Index, Middle, Ring, Little;
+
+SoftwareSerial mySerial(10, 11);
+
+int ThumbPos, IndexPos, MiddlePos, RingPos, LittlePos;
+
+int fingerSpeed = 20, HandSpeed = 20;
+
+int BLED = 13;
+int RLED = 12;
+int PUSH = 8;
+int SWITCH = 9;
+
+void setup()
+{
+  mySerial.begin(9600);
+  
+  pinMode(BLED, OUTPUT);
+  pinMode(RLED, OUTPUT);
+  pinMode(PUSH, INPUT_PULLUP);
+  pinMode(SWITCH, INPUT_PULLUP);
+
+  Thumb.attach(2);
+  Index.attach(3);
+  Middle.attach(4);
+  Ring.attach(5);
+  Little.attach(6);
+
+  Thumb.write(45);
+  ThumbPos = 45;
+  Index.write(45);
+  IndexPos = 45;
+  Middle.write(45);
+  MiddlePos = 45;
+  Ring.write(45);
+  RingPos = 45;
+  Little.write(45);
+  LittlePos = 45;
+
+  digitalWrite(RLED, LOW);
+  digitalWrite(BLED, LOW);
+}
+
+void loop()
+{
+  int ProgSwitch;
+  ProgSwitch = digitalRead(SWITCH);
+  if(ProgSwitch == 0)
+    {
+      PUSH_BUTTON();
+      digitalWrite(RLED, LOW);
+      digitalWrite(BLED, HIGH);
+    }
+
+  else if(ProgSwitch == 1)
+    {
+      BLUETOOTH();
+      digitalWrite(RLED, HIGH);
+      digitalWrite(BLED, LOW);
+    }
+}
+
+void PUSH_BUTTON()
+{
+  int PushState,i;
+  PushState = digitalRead(PUSH);
+  if(PushState == 0)
+    {
+      if(ThumbPos == 45 && IndexPos == 45 && MiddlePos == 45 && RingPos == 45 && LittlePos ==45)
+        {
+          for(i=45; i<=150; i++)
+            {
+              Index.write(i);
+              Middle.write(i);
+              Ring.write(i);
+              Little.write(i);
+
+              IndexPos = i;
+              MiddlePos = i;
+              RingPos = i;
+              LittlePos = i;
+              delay(HandSpeed);
+            }
+
+          for(i=45; i<=150; i++)
+            {
+              Thumb.write(i);
+              ThumbPos = i;
+              delay(HandSpeed);
+            }
+        }
+      else if(ThumbPos == 150 && IndexPos == 150 && MiddlePos == 150 && RingPos == 150 && LittlePos == 150)
+        {
+          for(i=150; i>=45; i--)
+            {
+              Thumb.write(i);
+              ThumbPos = i;
+              delay(HandSpeed);
+            }
+          
+          for(i=150; i>=45; i--)
+            {
+              Index.write(i);
+              Middle.write(i);
+              Ring.write(i);
+              Little.write(i);
+
+              IndexPos = i;
+              MiddlePos = i;
+              RingPos = i;
+              LittlePos = i;
+              delay(HandSpeed);
+            }
+        }
+    }
+
+  else if(PushState == 1)
+    {
+      Thumb.write(ThumbPos);
+      Index.write(IndexPos);
+      Middle.write(MiddlePos);
+      Ring.write(RingPos);
+      Little.write(LittlePos);
+    }
+}
+
+void BLUETOOTH()
+{
+  char data;
+  int i;
+  if(mySerial.available())
+    {
+      data = mySerial.read();
+      if(data == 'T')
+        {
+          if(ThumbPos == 45)
+            {
+              for(i=45; i<=150; i++)
+                {
+                  Thumb.write(i);
+                  ThumbPos = i;
+                  delay(fingerSpeed);
+                }
+            }
+
+          else if(ThumbPos == 150)
+            {
+              for(i=150; i>=45; i--)
+                {
+                  Thumb.write(i);
+                  ThumbPos = i;
+                  delay(fingerSpeed);
+                }
+            }
+        }
+      
+      else if(data == 'I')
+        {
+          if(IndexPos == 45)
+            {
+              for(i=45; i<=150; i++)
+                {
+                  Index.write(i);
+                  IndexPos = i;
+                  delay(fingerSpeed);
+                }
+            }
+
+          else if(IndexPos == 150)
+            {
+              for(i=150; i>=45; i--)
+                {
+                  Index.write(i);
+                  IndexPos = i;
+                  delay(fingerSpeed);
+                }
+            }
+        }
+
+      else if(data == 'M')
+        {
+          if(MiddlePos == 45)
+            {
+              for(i=45; i<=150; i++)
+                {
+                  Middle.write(i);
+                  MiddlePos = i;
+                  delay(fingerSpeed);
+                }
+            }
+
+          else if(MiddlePos == 150)
+            {
+              for(i=150; i>=45; i--)
+                {
+                  Middle.write(i);
+                  MiddlePos = i;
+                  delay(fingerSpeed);
+                }
+            }
+        }
+
+      else if(data == 'R')
+        {
+          if(RingPos == 45)
+            {
+              for(i=45; i<=150; i++)
+                {
+                  Ring.write(i);
+                  RingPos = i;
+                  delay(fingerSpeed);
+                }
+            }
+
+          else if(RingPos == 150)
+            {
+              for(i=150; i>=45; i--)
+                {
+                  Ring.write(i);
+                  RingPos = i;
+                  delay(fingerSpeed);
+                }
+            }
+        }
+
+      else if(data == 'L')
+        {
+          if(LittlePos == 45)
+            {
+              for(i=45; i<=150; i++)
+                {
+                  Little.write(i);
+                  LittlePos = i;
+                  delay(fingerSpeed);
+                }
+            }
+
+          else if(LittlePos == 150)
+            {
+              for(i=150; i>=45; i--)
+                {
+                  Little.write(i);
+                  LittlePos = i;
+                  delay(fingerSpeed);
+                }
+            }
+        }
+
+      else if(data == 'H')
+        {
+          if(ThumbPos == 45 && IndexPos == 45 && MiddlePos == 45 && RingPos == 45 && LittlePos == 45)
+            {
+              for(i=45; i<=150; i++)
+                {
+                  Index.write(i);
+                  Middle.write(i);
+                  Ring.write(i);
+                  Little.write(i);
+                  IndexPos = i;
+                  MiddlePos = i;
+                  RingPos = i;
+                  LittlePos = i;
+                  delay(HandSpeed);
+                }
+
+              for(i=45; i<=150; i++)
+                {
+                  Thumb.write(i);
+                  ThumbPos = i;
+                  delay(HandSpeed);
+                }
+            }
+
+          else if(ThumbPos == 150 && IndexPos == 150 && MiddlePos == 150 && RingPos == 150 && LittlePos == 150)
+            {
+              for(i=150; i>=45; i--)
+                {
+                  Thumb.write(i);
+                  ThumbPos = i;
+                  delay(HandSpeed);
+                }
+
+              for(i=150; i>=45; i--)
+                {
+                  Index.write(i);
+                  Middle.write(i);
+                  Ring.write(i);
+                  Little.write(i);
+                  IndexPos = i;
+                  MiddlePos = i;
+                  RingPos = i;
+                  LittlePos = i;
+                  delay(HandSpeed);
+                }
+            }
+          else
+            {
+              mySerial.println("Err: Position of all fingers are not same");
+            }
+        }
+    }  
+}
---
+++ README.txt
@@ -0,0 +1,15 @@
+Material to be used - PLA
+
+Print wristarge, wristsmall, Thumb, with an infill of 30%, wall thickness 2mm, best with no support, no raft.
+
+Print index3, majeure3, ringfinger, auriculaire3, with an infill of 30%, wall thickness 1.5mm, best with no support,
+no raft, no brim.
+
+Print robpart2, robpart3, robpart4, robpart5 with an infill of 30%, wall thickness 2mm, best with brim, no raft, no
+support.
+
+Print coverfinger with an infill of 30%, wall thickness 2mm, with support. To get the best printing result on the covers is to print them standing up, instead laying them flat.
+
+The wrist parts are good printed with an infill of 30%, wall thickness 2mm, with no raft, no brim, no support. The Gears of the wrist should be printed with the best quality your printer can give you.
+
+Box-Full print with 4x4x4 walls/top/bottom,Infil to add weight, supports needed for spade inset
